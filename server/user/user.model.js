91deeb8828cce1e856fc17013c2615e6a6b9eac5
muratozbay <muratozbay89@gmail.com>
2018-05-09 15:21:42 +0300

    - hash deleted

--- server/user/user.model.js
+++ server/user/user.model.js
@@ -8,7 +8,7 @@ const bcrypt = require('bcrypt');
  */
 
 
-const SALT_WORK_FACTOR = 10;
+// const SALT_WORK_FACTOR = 10;
 
 const UserSchema = new mongoose.Schema({
   username: {
@@ -34,30 +34,31 @@ const UserSchema = new mongoose.Schema({
     default: Date.now
   }
 });
+
 // generate a salt
 // eslint-disable-next-line consistent-return
-UserSchema.pre('save', function dummy(next) {
-  const user = this;
-
-// only hash the password if it has been modified (or is new)
-  if (!user.isModified('password')) return next();
-
-// generate a salt
-  // eslint-disable-next-line consistent-return
-  bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
-    if (err) return next(err);
-
-    // hash the password along with our new salt
-    // eslint-disable-next-line
-    bcrypt.hash(user.password, salt, (err, hash) => {
-      if (err) return next(err);
-
-      // override the cleartext password with the hashed one
-      user.password = hash;
-      next();
-    });
-  });
-});
+// UserSchema.pre('save', function dummy(next) {
+//   const user = this;
+//
+// // only hash the password if it has been modified (or is new)
+//   if (!user.isModified('password')) return next();
+//
+// // generate a salt
+//   // eslint-disable-next-line consistent-return
+//   bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+//     if (err) return next(err);
+//
+//     // hash the password along with our new salt
+//     // eslint-disable-next-line
+//     bcrypt.hash(user.password, salt, (err, hash) => {
+//       if (err) return next(err);
+//
+//       // override the cleartext password with the hashed one
+//       user.password = hash;
+//       next();
+//     });
+//   });
+// });
 /**
  * Add your
  * - pre-save hooks
